816e3133c730a9308037bc1866c04fd5cfb80b89
rizkimul <riizkimaul4@gmail.com>
2025-04-15 10:28:30 +0700

    create resource routes for notes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::resource('note', NoteController::class);
