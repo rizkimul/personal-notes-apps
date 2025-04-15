b085c8138ffc8efbf9c620885110d9bba7d68d77
rizkimul <riizkimaul4@gmail.com>
2025-04-15 12:11:24 +0700

    implement csrf and override method

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -2,6 +2,8 @@
     <div class="note-container single-note">
         <h1>Edit your note</h1>
         <form action="{{ route('note.update', $note) }}" method="POST" class="note">
+            @csrf
+            @method('PUT')
             <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here">
                 {{ $note->note }}
             </textarea>
