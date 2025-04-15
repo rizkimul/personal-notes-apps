e0162c5c6e8254a37bfd02a4aa41125ba4dc2b1f
rizkimul <riizkimaul4@gmail.com>
2025-04-15 11:50:19 +0700

    adjust view with new layout, add utility for button and read data

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-layout>
+    <div class="note-container single-note">
+        <h1>Create new note</h1>
+        <form action="{{ route('note.store') }}" method="POST" class="note">
+            <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here"></textarea>
+            <div class="note-buttons">
+                <a href="{{ route('note.index') }}" class="note-cancel=button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
+</x-layout>
--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-layout>
+    <div class="note-container single-note">
+        <h1>Edit your note</h1>
+        <form action="{{ route('note.update', $note) }}" method="POST" class="note">
+            <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here">
+                {{ $note->note }}
+            </textarea>
+            <div class="note-buttons">
+                <a href="{{ route('note.index') }}" class="note-cancel-button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
+</x-layout>
