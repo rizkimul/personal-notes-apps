5032f200176acf0c79c168846fe13b3a2d865788
rizkimul <riizkimaul4@gmail.com>
2025-04-16 14:56:47 +0700

    install breeze

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,6 +1,6 @@
-<x-layout>
+<x-app-layout>
     <div class="note-container single-note">
-        <h1>Edit your note</h1>
+        <h1 class="text-3xl py-4">Edit your note</h1>
         <form action="{{ route('note.update', $note) }}" method="POST" class="note">
             @csrf
             @method('PUT')
@@ -13,4 +13,4 @@
             </div>
         </form>
     </div>
-</x-layout>
+</x-app-layout>
